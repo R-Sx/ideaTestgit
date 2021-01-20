5c77eb6fc6e5582033419a9332cddd5440d12db2
suixin <747282898@qq.com>
2021-01-20 16:51:00 +0800

    hello world

---
+++ src/main/java/com/rsx/App.java
@@ -0,0 +1,13 @@
+package com.rsx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
